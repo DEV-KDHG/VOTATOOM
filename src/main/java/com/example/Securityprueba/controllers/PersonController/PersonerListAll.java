a8b31052950792426f12362535f8770218667e62
JhonM11 <jhonfremorales500@gmail.com>
2024-05-07 01:38:10 -0500

    Se agregar @CrossOrigin all a todos los endpoint de person y comptroller #2

--- src/main/java/com/example/Securityprueba/controllers/PersonController/PersonerListAll.java
+++ src/main/java/com/example/Securityprueba/controllers/PersonController/PersonerListAll.java
@@ -4,12 +4,13 @@ import com.example.Securityprueba.Dto.PersoneroDto.PersoneroDTO;
 import com.example.Securityprueba.service.PersoneroServices.PersonerosServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api/v1/personero")
 public class PersonerListAll {
