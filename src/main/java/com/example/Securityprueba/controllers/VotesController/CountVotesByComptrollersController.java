28dac1ec56602a07e38708b278872303ab591a57
Karol Hernandez <139244964+DEV-KDHG@users.noreply.github.com>
2024-05-27 22:39:34 -0500

    update

--- src/main/java/com/example/Securityprueba/controllers/VotesController/CountVotesByComptrollersController.java
+++ src/main/java/com/example/Securityprueba/controllers/VotesController/CountVotesByComptrollersController.java
@@ -3,6 +3,7 @@ package com.example.Securityprueba.controllers.VotesController;
 import com.example.Securityprueba.service.votesServices.VotesSeriviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins ="http://localhost:5173/")
 @RequestMapping("/votes")
 public class CountVotesByComptrollersController {
 
--- src/main/java/com/example/Securityprueba/controllers/VotesController/CountVotesByRepresentativesOrderByGradoDescController.java
+++ src/main/java/com/example/Securityprueba/controllers/VotesController/CountVotesByRepresentativesOrderByGradoDescController.java
@@ -4,6 +4,7 @@ package com.example.Securityprueba.controllers.VotesController;
 import com.example.Securityprueba.service.votesServices.VotesSeriviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins ="http://localhost:5173/")
 @RequestMapping("/votes")
 public class CountVotesByRepresentativesOrderByGradoDescController {
 
